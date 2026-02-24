422631dcc69c88974fd4465d7f81dac8c9de9b71
Bennet <bennetweith@yahoo.com>
2026-02-24 18:19:43 +0100

    Add documentation for find_free_slot_index function in parking_garage.c

--- source/parking_garage.c
+++ source/parking_garage.c
@@ -1,4 +1,8 @@
 #include "parking_garage.h"
+
+/**
+ * @brief Sucht einen freien Stellplatz und liefert dessen Index zurück.
+ */
 static int find_free_slot_index(const ParkingGarage *p_garage);
 static int find_slot_by_license_plate(const ParkingGarage *p_garage,const char *p_license_plate);
 static void assign_slot(ParkingSlot *p_slot,const Vehicle *p_vehicle,int current_time);
