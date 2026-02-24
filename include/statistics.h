a13d86854dd1738990d3d2d86f537eb133fb6942
Bennet <bennetweith@yahoo.com>
2026-02-24 10:47:40 +0100

    Refactor statistics.h to improve organization and clarity of function declarations

--- include/statistics.h
+++ include/statistics.h
@@ -1,6 +1,11 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
-
+#include "parking_garage.h"
 //Hier Programm:
-
+void statistics_init(Statistics *p_statistics);
+void statistics_on_queued(Statistics *p_statistics);
+void statistics_on_parked_from_queue(Statistics *p_statistics,int wait_duration);
+void statistics_on_departure(Statistics *p_statistics,int park_duration);
+void statistics_step_update(Statistics *p_statistics,int occupied_slots,int total_slots);
+void statistics_print(const Statistics *p_statistics);
 #endif /* STATISTICS_H */
