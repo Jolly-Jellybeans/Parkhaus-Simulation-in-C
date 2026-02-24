ac12029d43acbee3d932f3518278729d1afd620f
Bennet <bennetweith@yahoo.com>
2026-02-24 18:20:13 +0100

    Add documentation for find_slot_by_license_plate function in parking_garage.c

--- source/parking_garage.c
+++ source/parking_garage.c
@@ -4,6 +4,9 @@
  * @brief Sucht einen freien Stellplatz und liefert dessen Index zurück.
  */
 static int find_free_slot_index(const ParkingGarage *p_garage);
+/**
+ * @brief Findet den Stellplatz-Index anhand des Fahrzeugkennzeichens.
+ */
 static int find_slot_by_license_plate(const ParkingGarage *p_garage,const char *p_license_plate);
 static void assign_slot(ParkingSlot *p_slot,const Vehicle *p_vehicle,int current_time);
 static void clear_slot(ParkingSlot *p_slot);
