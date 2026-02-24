220248cfec749276babd62f50cefd565a89b81f0
Bennet <bennetweith@yahoo.com>
2026-02-24 18:10:46 +0100

    Add documentation for queue_dequeue function in queue.h

--- include/queue.h
+++ include/queue.h
@@ -23,6 +23,13 @@ void queue_destroy(Queue *p_queue);
  * @return true bei Erfolg, false wenn die Queue voll ist.
  */
 bool queue_enqueue(Queue *p_queue,const Vehicle *p_vehicle);
+/**
+ * @brief Entfernt das vorderste Fahrzeug aus der Warteschlange.
+ *
+ * @param[in,out] p_queue Queue, aus der das Fahrzeug entfernt wird.
+ * @param[out] p_out_vehicle Zielstruktur für das entfernte Fahrzeug.
+ * @return true bei Erfolg, false wenn die Queue leer ist.
+ */
 bool queue_dequeue(Queue *p_queue,Vehicle *p_out_vehicle);
 bool queue_is_empty(const Queue *p_queue);
 #endif /* QUEUE_H */
