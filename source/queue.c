f188095bb0fcd9cbdfde17565ca80a495db64520
Bennet <bennetweith@yahoo.com>
2026-02-24 18:21:59 +0100

    Add documentation for node_destroy function in queue.c

--- source/queue.c
+++ source/queue.c
@@ -4,4 +4,7 @@
  * @brief Erzeugt einen neuen Listenknoten mit den Fahrzeugdaten.
  */
 static QueueNode *node_create(const Vehicle *p_vehicle);
+/**
+ * @brief Gibt einen Listenknoten inkl. Fahrzeugdaten frei.
+ */
 static void node_destroy(QueueNode *p_node);
