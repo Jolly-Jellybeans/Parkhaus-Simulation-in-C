5d3006359bad470728c5bc3624dc221144962dd7
Bennet <bennetweith@yahoo.com>
2026-02-24 18:21:38 +0100

    Add documentation for node_create function in queue.c

--- source/queue.c
+++ source/queue.c
@@ -1,3 +1,7 @@
 #include "queue.h"
+
+/**
+ * @brief Erzeugt einen neuen Listenknoten mit den Fahrzeugdaten.
+ */
 static QueueNode *node_create(const Vehicle *p_vehicle);
 static void node_destroy(QueueNode *p_node);
