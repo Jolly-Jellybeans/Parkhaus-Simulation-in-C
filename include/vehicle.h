39181e1af22d9f5158af8d43f0918e23f81e07d9
Bennet <bennetweith@yahoo.com>
2026-02-24 18:14:43 +0100

    Add documentation for vehicle_is_valid function in vehicle.h

--- include/vehicle.h
+++ include/vehicle.h
@@ -7,6 +7,12 @@ typedef struct
   int remaining_duration;
   int entry_time;
 } Vehicle;
+/**
+ * @brief Prüft, ob eine Fahrzeugstruktur gültige Werte enthält.
+ *
+ * @param[in] p_vehicle Zu überprüfendes Fahrzeug.
+ * @return true, wenn das Fahrzeug validiert werden kann, sonst false.
+ */
 bool vehicle_is_valid(const Vehicle *p_vehicle);
 bool vehicle_has_license_plate(const Vehicle *p_vehicle,const char *p_license_plate);
 #endif
