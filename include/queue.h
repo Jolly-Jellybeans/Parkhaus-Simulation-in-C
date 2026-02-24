9de92fe5528cfd5a6b7c20aa5417e34d5aa88f58
Bennet <bennetweith@yahoo.com>
2026-02-24 18:10:03 +0100

    Add documentation for queue_destroy function in queue.h

--- include/queue.h
+++ include/queue.h
@@ -8,6 +8,12 @@
  * @return Zeiger auf die erzeugte Queue oder NULL bei Misserfolg.
  */
 Queue *queue_create(void);
+/**
+ * @brief Gibt die Queue und ihre Ressourcen frei.
+ *
+ * @param[in,out] p_queue Zu zerstörende Queue.
+ * @return Kein Rückgabewert.
+ */
 void queue_destroy(Queue *p_queue);
 bool queue_enqueue(Queue *p_queue,const Vehicle *p_vehicle);
 bool queue_dequeue(Queue *p_queue,Vehicle *p_out_vehicle);
