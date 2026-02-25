772ff30818182510a935c2b8cb36ea1361baa4f5
Bennet <bennetweith@yahoo.com>
2026-02-25 21:47:55 +0100

    Remove unused assign_slot function declaration in parking_garage.c

--- source/parking_garage.c
+++ source/parking_garage.c
@@ -8,10 +8,7 @@ static int find_free_slot_index(const ParkingGarage *p_garage);
  * @brief Findet den Stellplatz-Index anhand des Fahrzeugkennzeichens.
  */
 static int find_slot_by_license_plate(const ParkingGarage *p_garage,const char *p_license_plate);
-/**
- * @brief Belegt einen Stellplatz mit den Informationen eines Fahrzeugs.
- */
-static void assign_slot(ParkingSlot *p_slot,const Vehicle *p_vehicle,int current_time);
+
 /**
  * @brief Leert einen Stellplatz und setzt ihn auf den Ausgangszustand zurück.
  */
