15c765cba72f9e6760a01ee801449cb794bf3c3b
arne_stuetzle <arne.stuetzle@icloud.com>
2026-03-01 15:30:07 +0100

    Add pseudo struct QueueNode

--- source/queue.c
+++ source/queue.c
@@ -8,3 +8,12 @@ static QueueNode *node_create(const Vehicle *p_vehicle);
  * @brief Gibt einen Listenknoten inkl. Fahrzeugdaten frei.
  */
 static void node_destroy(QueueNode *p_node);
+
+/*
+// Definition eines einzelnen Knotens (QueueNode)
+STRUCTURE QueueNode
+    data: Vehicle              // Die gespeicherten Fahrzeugdaten
+    next: Zeiger auf QueueNode // Verweis auf das nächste Element
+END STRUCTURE
+
+*/
