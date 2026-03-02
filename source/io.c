425ba5fd0055792c011495b939dd77fef865c1ca
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-02 14:57:41 +0100

    Add pseudocode for input validation in io module
    - Added menu input validation pseudocode
    - Added vehicle data validation pseudocode

--- source/io.c
+++ source/io.c
@@ -4,3 +4,41 @@
  * @brief Entfernt verbleibende Zeichen aus dem Eingabepuffer, um saubere Lesefolgen zu gewährleisten.
  */
 static void clear_input_buffer(void);
+
+/*
+---------------------------------------------
+  PSEUDOCODE - Input validation
+---------------------------------------------
+
+FUNCTION ValidateMenuInput(input)
+
+       IF input is not numeric THEN
+           DISPLAY error message
+           RETURN invalid
+       END IF
+
+       IF input < 0 OR input > 4 THEN
+           DISPLAY "Invalid menu option"
+           RETURN invalid
+       END IF
+
+       RETURN valid
+
+   END FUNCTION
+
+
+   FUNCTION ValidateVehicleData(vehicle)
+
+       IF vehicle.id is invalid THEN
+           RETURN invalid
+       END IF
+
+       IF vehicle.remaining_duration <= 0 THEN
+           RETURN invalid
+       END IF
+
+       RETURN valid
+
+   END FUNCTION
+
+*/
