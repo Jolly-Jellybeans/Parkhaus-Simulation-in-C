5fd2eff1b1fa2dfa2425d63b0e5c5d8ecf1c6299
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-02 21:42:56 +0100

    Add pseudocode for menu input validation
    Added pseudocode for input validation in io.c.

--- source/io.c
+++ source/io.c
@@ -3,4 +3,29 @@
 /**
  * @brief Entfernt verbleibende Zeichen aus dem Eingabepuffer, um saubere Lesefolgen zu gewährleisten.
  */
+
+/*
+---------------------------------------------
+  PSEUDOCODE - Input validation
+---------------------------------------------
+
+FUNCTION ValidateMenuInput(input)
+
+       IF input is not numeric THEN
+           DISPLAY error message
+           RETURN invalid
+       END IF
+
+       IF input < 0 OR input > 4 THEN
+           DISPLAY "Invalid menu option"
+           RETURN invalid
+       END IF
+
+       RETURN valid
+
+   END FUNCTION
+*/
+
+
 static void clear_input_buffer(void);
+
