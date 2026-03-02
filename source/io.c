b2a104c25149025109a787f9f796f91afb3e3828
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-02 21:51:53 +0100

    Add comments

--- source/io.c
+++ source/io.c
@@ -9,16 +9,16 @@
   PSEUDOCODE - Input validation
 ---------------------------------------------
 
-FUNCTION ValidateMenuInput(input)
+FUNCTION ValidateMenuInput(input)            // Function checks if menu input is valid
 
-       IF input is not numeric THEN
-           DISPLAY error message
-           RETURN invalid
+       IF input is not numeric THEN          // If user did not enter a number
+           DISPLAY error message             // Show error message
+           RETURN invalid                    // Stop and mark as invalid
        END IF
 
-       IF input < 0 OR input > 4 THEN
-           DISPLAY "Invalid menu option"
-           RETURN invalid
+       IF input < 0 OR input > 4 THEN        // If number is outside allowed range
+           DISPLAY "Invalid menu option"     // Tell user the opinion is wrong
+           RETURN invalid                    // Mark input as invalid
        END IF
 
        RETURN valid
@@ -27,14 +27,14 @@ FUNCTION ValidateMenuInput(input)
 */
 
 /*
-   FUNCTION ValidateVehicleData(vehicle)
+   FUNCTION ValidateVehicleData(vehicle)                  // Function checks if vehicle data is correct
 
-       IF vehicle.id is invalid THEN
-           RETURN invalid
+       IF vehicle.id is invalid THEN                      // Check if vehicle ID is wrong or empty
+           RETURN invalid                                 // Stop and mark vehicle as invalid
        END IF
 
-       IF vehicle.remaining_duration <= 0 THEN
-           RETURN invalid
+       IF vehicle.remaining_duration <= 0 THEN            // Check if parking time is zero
+           RETURN invalid                                 // Mark vehicle as invalid
        END IF
 
        RETURN valid
