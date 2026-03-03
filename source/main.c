9090c2c82f7150d53be587c80146a2ca48f24b35
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-03 18:51:51 +0100

    Add pseudocode for main program flow
    Added pseudocode comments for main program flow.

--- source/main.c
+++ source/main.c
@@ -5,7 +5,7 @@
 #include "io.h"
 
 
-
+/*
 // -----------------------------------------------
 // PSEUDOCODE
 //------------------------------------------------
@@ -55,3 +55,43 @@ BEGIN Menu Logic
     TERMINATE program
 
 END PROGRAM
+*/
+
+/*
+------------------------------------------------------
+    PSEUDOCODE - Main program flow
+------------------------------------------------------
+
+FUNCTION main
+
+  // 1. CONFIGURATION
+  // Create config struct and fill it from user input
+  config <- CALL get_config_from_user()
+
+  // 2. INITIALIZATION
+  CALL srand(config.seed)
+
+  garage <- CALL parking_garage_create(config.slots)
+  IF garage is NULL THEN
+      RETURN -1
+  END IF
+
+  stats <- empty Statistics structure
+  CALL statistics_init(ADDRESS OF stats)
+
+  // 3. SIMULATION
+  // Pass values from config struct into simulation
+  CALL simulation(garage,
+                  ADDRESS OF stats,
+                  config.sim_duration,
+                  config.arrival_prob,
+                  config.max_park_duration)
+
+  // 4. END
+  CALL print_result_message(ADDRESS OF stats)
+  CALL parking_garage_destroy(garage)
+
+  RETURN 0
+
+END FUNCTION
+*/
