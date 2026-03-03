edac713ce670eb537e1f0adcf8f357a1c5f1ea68
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-03 17:14:58 +0100

    Remove pseudocode for parameter validation
    Removed pseudocode for reading simulation parameters from io.c.

--- source/io.c
+++ source/io.c
@@ -95,52 +95,10 @@ END FUNCTION
   END FUNCTION
 */
 /*
-FUNCTION read_simulation_parameters
-    // slot_count
-    DO
-        READ slot_count
-        IF slot_count is not numeric OR slot_count <= 0 THEN
-            PRINT "Slot count must be a positive number."
-        END IF
-    WHILE slot_count is invalid                      //repeat until valid
-
-    // max_parking_duration
-    DO
-        READ max_duration
-        IF max_duration is not numeric OR max_duration <= 0 THEN
-            PRINT "Max parking duration must be positive."
-        END IF
-    WHILE max_duration is invalid
-
-    // simulation_duration (steps)
-    DO
-        READ sim_steps
-        IF sim_steps is not numeric OR sim_steps <= 0 THEN
-            PRINT "Simulation duration must be positive."
-        END IF
-    WHILE sim_steps is invalid
-
-    // arrival_probability (0..100)
-    DO
-        READ arrival_probability
-        IF arrival_probability is not numeric OR arrival_probability < 0 OR arrival_probability > 100 THEN
-            PRINT "Arrival probability must be between 0 and 100."
-        END IF
-    WHILE arrival_probability is invalid
-
-    // random_seed
-    DO
-        READ seed
-        IF seed is not numeric OR seed < 0 THEN
-            PRINT "Seed must be a non-negative number."
-        END IF
-    WHILE seed is invalid
-
-    RETURN all parameters                            // return validated values
-END FUNCTION
+-------------------------------------------------------
+  END OF PSEUDOCODE
+-------------------------------------------------------
 */
-
-
 
 static void clear_input_buffer(void);
 
