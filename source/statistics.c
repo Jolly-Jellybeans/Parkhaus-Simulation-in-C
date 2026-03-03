1513a49a2e11d6a62db952325ad2198f5d515313
Bennet <bennetweith@yahoo.com>
2026-03-03 07:42:12 +0100

    Implement statistics_init

--- source/statistics.c
+++ source/statistics.c
@@ -1,1 +1,22 @@
 #include "statistics.h"
+FUNCTION statistics_init(p_statistics)
+
+    IF p_statistics = NULL
+    THEN
+        RETURN
+    END IF
+
+    p_statistics.currently_parked ← 0
+
+    p_statistics.occupancy_ratio_sum ← 0.0
+    p_statistics.occupancy_samples ← 0
+
+    p_statistics.queued_vehicle_count_cumulative ← 0
+    p_statistics.queued_vehicle_count_served ← 0
+
+    p_statistics.total_park_duration ← 0
+    p_statistics.departed_vehicle_count ← 0
+
+    p_statistics.total_wait_duration ← 0
+
+END FUNCTION
