943e25b1265261a646438efe637af40fdb380d50
Bennet <bennetweith@yahoo.com>
2026-03-03 20:39:18 +0100

    Updated use of pointer and changed some comments into english

--- source/main.c
+++ source/main.c
@@ -13,32 +13,33 @@
 
 FUNCTION main
 
-  // 1. CONFIGURATION
-  // Create config struct and fill it from user input
-  config <- CALL get_config_from_user()
+  // 1. KONFIGURATION
+  // Simulationsparameter vom Benutzer abfragen.
+  config ← CALL get_config_from_user()
 
-  // 2. INITIALIZATION
+  // 2. INITIALISIERUNG
   CALL srand(config.seed)
 
-  garage <- CALL parking_garage_create(config.slots)
-  IF garage is NULL THEN
+  p_garage ← CALL parking_garage_create(config.slots)
+  IF p_garage ist NULL THEN
       RETURN -1
   END IF
 
-  stats <- empty Statistics structure
-  CALL statistics_init(ADDRESS OF stats)
+  stats ← leere Statistics-Struktur
+  CALL statistics_init(ADRESSE VON stats)
 
   // 3. SIMULATION
-  // Pass values from config struct into simulation
-  CALL simulation(garage,
-                  ADDRESS OF stats,
+  // Simulationsparameter aus config-Struktur übergeben.
+  CALL simulation(p_garage,
+                  ADRESSE VON stats,
                   config.sim_duration,
                   config.arrival_prob,
                   config.max_park_duration)
 
-  // 4. END
-  CALL print_result_message(ADDRESS OF stats)
-  CALL parking_garage_destroy(garage)
+  // 4. ENDE
+  // Gesamtstatistik ausgeben und Ressourcen freigeben.
+  CALL statistics_print(ADRESSE VON stats)
+  CALL parking_garage_destroy(p_garage)
 
   RETURN 0
 
