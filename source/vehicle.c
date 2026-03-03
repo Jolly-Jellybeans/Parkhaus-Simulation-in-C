daf8c8f3d83f97fc28925c25962df10c850d373d
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-03 19:23:02 +0100

    Update vehicle.c

--- source/vehicle.c
+++ source/vehicle.c
@@ -1,28 +1,2 @@
 #include "vehicle.h"
 
-/*
--------------------------------------
-  PSEUDOCODE -- Vehicle Validation
--------------------------------------
-
-FUNCTION vehicle_is_valid(vehicle)
-  IF vehicle is Null THEN
-        RETURN false                                  //no vehicle given
-  END IF
-
-  IF vehicle.id <= 0 THEN
-        RETURN false                                  // id must be positive
-  END IF
-
-  IF vehicle.remaining_duration <= 0 THEN
-        RETURN false                                  // parking duration must be positive
-  END IF
-
-  IF vehicle.entry_time < THEN
-        RETURN false                                  // entry time cannot be negative
-  END IF
-
-  RETURN true                                         // vehicle data is valid
-
-END FUNKTION
-*/
