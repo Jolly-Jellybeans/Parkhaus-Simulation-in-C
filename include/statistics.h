4f6d358f92fbe82b7349eb2d6ca82cfb1abe0dd1
Bennet <bennetweith@yahoo.com>
2026-03-03 17:26:44 +0100

    Updated struct statistics and statistics.h with new statistics

--- include/statistics.h
+++ include/statistics.h
@@ -5,9 +5,14 @@
 STRUCTURE Statistics
 
     currently_parked
+    currently_queued
+
+    parked_vehicle_count_sum
+    queued_vehicle_count_sum
+    time_samples
+
     occupancy_ratio_sum
     occupancy_samples
-    queued_vehicle_count_cumulative
     queued_vehicle_count_served
     total_park_duration
     departed_vehicle_count
@@ -42,13 +47,14 @@ void statistics_on_parked_from_queue(Statistics *p_statistics,int wait_duration)
  */
 void statistics_on_departure(Statistics *p_statistics,int park_duration);
 /**
- * @brief Aktualisiert zeitabhängige Werte wie Auslastung in jedem Simulationsschritt.
+ * @brief Aktualisiert zeitabhängige Werte (Parkende, Auslastung, Wartende) pro Simulationsschritt.
  *
  * @param[in,out] p_statistics Statistikstruktur, die aktualisiert wird.
  * @param[in] occupied_slots Aktuell belegte Stellplätze.
  * @param[in] total_slots Gesamtanzahl verfügbarer Stellplätze.
+ * @param[in] queued_vehicles Aktuell wartende Fahrzeuge in der Warteschlange.
  */
-void statistics_step_update(Statistics *p_statistics,int occupied_slots,int total_slots);
+void statistics_step_update(Statistics *p_statistics,int occupied_slots,int total_slots,int queued_vehicles);
 /**
  * @brief Gibt die gesammelten Statistikwerte auf der Konsole aus.
  *
