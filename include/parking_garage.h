d0c50940f168e6f58ec6dc370cb067fd1efdca8c
Niklas Reutter <niklasreutter107@gmail.com>
2026-03-08 22:30:59 +0100

    Update parking_garage.h

--- include/parking_garage.h
+++ include/parking_garage.h
@@ -5,14 +5,14 @@
 
 // Hier Programm:
 
-/* typedef struct
+typedef struct
 {
     int slot_count;          // Gesamtanzahl der Stellplätze
     int occupied_count;      // Anzahl aktuell belegter Stellplätze
     Vehicle *p_slots;        // Dynamisches Array: Stellplätze
     Queue *p_queue;          // Warteschlange für Fahrzeuge
 } ParkingGarage;
-*/
+
 /*
 TYPE Parking Garage
     slot_count               // Gesamtanzahl der Stellplätze
