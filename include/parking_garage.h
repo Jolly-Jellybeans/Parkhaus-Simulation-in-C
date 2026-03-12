a99f356ceef8b9c3beacf53820e741af7c427c9b
arne_stuetzle <arne.stuetzle@icloud.com>
2026-03-12 14:59:12 +0100

    Add struct ParkingSlot

--- include/parking_garage.h
+++ include/parking_garage.h
@@ -2,8 +2,27 @@
 #define PARKING_GARAGE_H
 #include "vehicle.h"
 #include "queue.h"
+#include <stdbool.h>
 
 // Hier Programm:
+
+/**
+ * @brief Repräsentiert einen einzelnen Stellplatz im Parkhaus.
+ */
+typedef struct
+{
+    Vehicle vehicle;         // Fahrzeugdaten des parkenden Fahrzeugs
+    int departure_time;      // Geplanter Abfahrtszeitpunkt
+    bool is_occupied;        // true, wenn der Stellplatz belegt ist
+} ParkingSlot;
+
+/*
+TYPE ParkingSlot
+    vehicle              // Fahrzeugdaten des parkenden Fahrzeugs
+    departure_time       // Geplanter Abfahrtszeitpunkt
+    is_occupied          // true, wenn der Stellplatz belegt ist
+END TYPE
+*/
 
 typedef struct
 {
