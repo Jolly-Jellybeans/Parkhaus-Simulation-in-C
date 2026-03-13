294c83b2383223df2de129dfd8091d898e4c23ec
arne_stuetzle <arne.stuetzle@icloud.com>
2026-03-13 17:16:36 +0100

    Change test

--- tests/test_vehicle.c
+++ tests/test_vehicle.c
@@ -22,35 +22,4 @@ void test_vehicle_is_valid_with_valid_vehicle(void)
 	assert(vehicle_is_valid(&v) == true);
 }
 
-/*
-Test 3:
-id <= 0 muss ungueltig sein.
-*/
-void test_vehicle_is_valid_with_invalid_id(void)
-{
-	Vehicle v = { .id = 0, .remaining_duration = 5, .entry_time = 0 };
-	assert(vehicle_is_valid(&v) == false);
-}
 
-/*
-Test 4:
-remaining_duration <= 0 muss ungueltig sein.
-*/
-void test_vehicle_is_valid_with_invalid_duration(void)
-{
-	Vehicle v_zero = { .id = 1, .remaining_duration = 0, .entry_time = 0 };
-	Vehicle v_negative = { .id = 1, .remaining_duration = -2, .entry_time = 0 };
-
-	assert(vehicle_is_valid(&v_zero) == false);
-	assert(vehicle_is_valid(&v_negative) == false);
-}
-
-/*
-Test 5:
-entry_time hat keinen Einfluss auf die Validierung.
-*/
-void test_vehicle_is_valid_ignores_entry_time(void)
-{
-	Vehicle v_negative_entry = { .id = 2, .remaining_duration = 3, .entry_time = -10 };
-	assert(vehicle_is_valid(&v_negative_entry) == true);
-}
